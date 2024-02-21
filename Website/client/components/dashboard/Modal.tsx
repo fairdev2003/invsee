045925045a38160353d9df2a297cad7e7bf110aa
Jakub Klimkiewicz <jakubklimkiewicz70@gmail.com>
2024-02-21 22:17:36 +0100

    Added better api routes, some components are now server side components.

--- Website/client/components/dashboard/Modal.tsx
+++ Website/client/components/dashboard/Modal.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { DialogTrigger } from "@radix-ui/react-dialog";
 import { Dialog, DialogContent } from "../ui/dialog";
@@ -21,13 +21,13 @@ export const AddItemModal = ({
   className,
   color,
 }: AddItemModalProps) => {
-
   const [itemName, setItemName] = useState<string>("");
   const [modTag, setModTag] = useState<string>("");
 
-  const [itemNameRef, ModTagRef] = [
+  const [itemNameRef, ModTagRef, descriptionRef] = [
     useRef<HTMLInputElement>(null),
-    useRef<HTMLInputElement>(null)
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLTextAreaElement>(null),
   ];
 
   return (
@@ -41,18 +41,28 @@ export const AddItemModal = ({
 
       <DialogContent className={cn("", className)}>
         <div className="flex justify-start gap-x-3 items-center">
-          <Image width={40} height={40} alt="deafult" src={`/mc_assets/${modTag.replace(":", "__").split("__")[0]}/${modTag}.png`} onError={(e) => {
-            e.currentTarget.src = `/mc_assets/ae2/ae2__controller.png`
-          }}></Image>
+          <Image
+            width={40}
+            height={40}
+            alt="deafult"
+            src={`/mc_assets/${
+              modTag.replace(":", "__").split("__")[0]
+            }/${modTag}.png`}
+            onError={(e) => {
+              e.currentTarget.src = `/mc_assets/ae2/ae2__controller.png`;
+            }}
+          ></Image>
           <p className="text-white font-medium">Add New Item!</p>
         </div>
         <div className="flex flex-col gap-5 items-center mb-5">
           <div className="flex gap-x-4 items-center justify-center">
-            <p className="text-white font-medium flex justify-center w-[100px]">Item Name</p>
+            <p className="text-white font-medium flex justify-center w-[100px]">
+              Item Name
+            </p>
             <div className="flex gap-3 items-center h-7 rounded-md bg-[#32343a] py-6 px-3 text-white font-[500] w-[300px]">
               <input
                 ref={itemNameRef}
-              className="bg-transparent outline-none w-full"
+                className="bg-transparent outline-none w-full"
                 type="text"
                 onChange={(e) => setItemName(e.target.value)}
                 placeholder={"Your Item Name"}
@@ -72,6 +82,18 @@ export const AddItemModal = ({
                 type="text"
                 placeholder={"Your Item Name"}
               ></input>
+            </div>
+          </div>
+          <div className="flex gap-x-4 items-center justify-center">
+            <p className="text-white font-medium flex justify-center w-[100px]">
+              Description
+            </p>
+            <div className="flex gap-3 items-center rounded-md bg-[#32343a] text-white font-[500] w-[300px] h-auto">
+              <textarea
+                ref={descriptionRef}
+                className="bg-transparent w-full h-full outline-none mx-3 my-6 max-h-[300px] min-h-[50px]"
+                placeholder="Description..."
+              />
             </div>
           </div>
         </div>
--- Website/client/components/wiki/WikiContent.tsx
+++ Website/client/components/wiki/WikiContent.tsx
@@ -3,20 +3,37 @@
 import { cn } from "@/lib/utils";
 import { useState, useEffect } from "react";
 import SliderMenu from "../SliderMenu";
+import { getAllItems, searchItems } from "@/actions/itemActions";
 
 interface WikiContentProps {
-  data: any
-  className?: string
+  data: any,
+  className?: string,
+  section?: string | undefined | null | string[]
 }
 
-export default function WikiContent({ data, className }: WikiContentProps) {
-    
+const sections = [
+    { name: "Overview", value: "overview", desc: "Overview of the item"},
+    { name: "Crafting", value: "crafting", desc: "Crafting recipe for the item"},
+    { name: "Smelting", value: "smelting", desc: "Smelting recipe for the item"},
+    { name: "Brewing", value: "brewing", desc: "Brewing recipe for the item"},
+    { name: "Usage", value: "usage", desc: "Usage of the item"},
+    { name: "History", value: "history", desc: "History of the item"},
+    { name: "Trivia", value: "trivia", desc: "Trivia of the item"},
+    { name: "Gallery", value: "gallery", desc: "Gallery of the item"},
+    { name: "Tutorials", value: "tutorials", desc: "Tutorials of the item"},
+    { name: "Links", value: "links", desc: "Links of the item"}
+]
 
+export default function WikiContent({ data, className, section }: WikiContentProps) {
 
     return (
         <section>
             <SliderMenu item_tag={data.tag_name}></SliderMenu>
-            
+            {sections?.filter(s => s.value === section).map((s, index) => {
+                return (<div key={index} className={cn("text-white")}>
+                    <p className="mt-5">{s.desc}</p>
+                </div>)
+            })}
         </section>
     )
 }
