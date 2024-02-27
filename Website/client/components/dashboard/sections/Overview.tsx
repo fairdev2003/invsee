d0dd9d674843824e6d89d7beca4df69923d95f39
Jakub Klimkiewicz <jakubklimkiewicz70@gmail.com>
2024-02-27 21:06:32 +0100

    Added Card for stats

--- Website/client/components/dashboard/sections/Overview.tsx
+++ Website/client/components/dashboard/sections/Overview.tsx
@@ -1,13 +1,18 @@
 import LogCard from "../cards/LogCard";
+import StatsCard from "../cards/StatsCard";
 import { UsersWithAccess } from "../cards/UsersWithAccess";
 
 export default function Overview() {
     return (
         <div>
-            <h1 className="text-2xl text-white font-[600]">Overview</h1>
+            <div className="flex gap-x-3">
+                <h1 className="text-2xl text-white font-[600]">Overview</h1>
+                chujec
+            </div>
             <div className="flex flex-wrap gap-5">
                 <UsersWithAccess/>
                 <LogCard/>
+                <StatsCard/>
             </div>
         </div>
     )
