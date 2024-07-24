3cce728335b86131b5d74403d3351154b6a4851b
Jakub Klimkiewicz <jakubklimkiewicz70@gmail.com>
2024-07-24 03:37:26 +0200

    s

--- Website/invsee/app/admin/workspace/page.tsx
+++ Website/invsee/app/admin/workspace/page.tsx
@@ -5,10 +5,9 @@ import FireIcon from "./(images)/modIcon.svg";
 import CraftingIcon from "./(images)/craftingIcon.svg";
 import UserIcon from "./(images)/userIcon.svg";
 import SelectWorkspaceCard from "./(components)/SelectWorkspaceCard";
-import { useState } from "react";
+import { motion } from "framer-motion"; 
 
 const Page = () => {
-  const [isSelected, selected] = useState<boolean>(false);
 
   return (
     <div className="mt-[130px] flex flex-col justify-center items-center px-5">
@@ -18,12 +17,12 @@ const Page = () => {
         </h1>
       </div>
 
-      <div className="flex flex-wrap flex-3 justify-center gap-5 mt-10 p-7 rounded-xl border-[1px] border-gray-500">
+      <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 1}} className="flex flex-wrap flex-3 justify-center gap-5 mt-10 p-7 rounded-xl border-[1px] border-gray-500">
         <SelectWorkspaceCard name="Create a mod" image={FireIcon} />
         <SelectWorkspaceCard name="Create an item" image={PickaxeIcon} />
         <SelectWorkspaceCard name="Create a crafting" image={CraftingIcon} />
         <SelectWorkspaceCard name="Add new user" image={UserIcon} />
-      </div>
+      </motion.div>
     </div>
   );
 };
