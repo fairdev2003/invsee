903153935b54b336f2eb2a3aa71f3253f4b057ff
Jakub Klimkiewicz <jakubklimkiewicz70@gmail.com>
2024-08-08 20:12:13 +0200

    stage

--- Website/invsee/app/admin/dashboard/page.tsx
+++ Website/invsee/app/admin/dashboard/page.tsx
@@ -7,21 +7,18 @@ import { useDashboardStore } from "./(stores)/dashboardStore";
 import Authentication from "./(components)/Authentication";
 import { useUserStore } from "@/stores/user_store";
 import { PermissionLevel } from "@/lib/types/userTypes";
-import { useEffect } from "react";
-import { Auth } from './index'
+import { Auth } from "./index";
 
 const Page = () => {
   const { selectedDashboardSection } = useDashboardStore();
   const { account_data } = useUserStore();
-
-  
   const permissionWithAccess: PermissionLevel[] = [
     "ACCESS_OVERVIEW",
     "ACCESS_MODS",
     "ACCESS_WORKSPACES",
     "ACCESS_ITEMS",
     "ACCESS_PENDING",
-    "ACCESS_USER_ROLES"
+    "ACCESS_USER_ROLES",
   ];
 
   const auth = new Auth(selectedDashboardSection, permissionWithAccess);
