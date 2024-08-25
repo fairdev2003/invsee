de6422a0107575f7063e692427833b7fbae1132c
Jakub Klimkiewicz <jakubklimkiewicz70@gmail.com>
2024-08-25 23:21:45 +0200

    co

--- Website/invsee/app/_trpc/serverClient.ts
+++ Website/invsee/app/_trpc/serverClient.ts
@@ -5,7 +5,7 @@ import { appRouter } from "@/server";
 export const serverClient = appRouter.createCaller({
   links: [
     httpBatchLink({
-      url: "https://invsee.vercel.app/api/trpc",
+      url: "/api/trpc",
     }),
   ],
 });
--- Website/invsee/app/api/trpc/[trpc]/route.ts
+++ Website/invsee/app/api/trpc/[trpc]/route.ts
@@ -1,5 +1,4 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-
 import { appRouter } from '@/server';
 import { createContext } from '@/server/trpc';
 import { NextRequest } from 'next/server';
